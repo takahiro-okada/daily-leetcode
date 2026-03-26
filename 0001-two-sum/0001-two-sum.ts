396eb9202cdaa0207ec6527a4e6180949e2d394b
oka <76928095+takahiro-okada@users.noreply.github.com>
2026-03-26 22:49:34 +1300

    Time: 62 ms (12.54%), Space: 55.3 MB (95.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    let hashMap = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if((nums[i] + nums[j] === target) && (i != j)) {
+                return [i,j];
+            }
+        }
+    }
+};
